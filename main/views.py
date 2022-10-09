868d2d635a2076dc88c0e9c55d1d672509474c33
Vodzinskiy <roma.vodzinskiy@gmail.com>
2022-10-09 17:12:38 +0300

    add user-notes

--- main/views.py
+++ main/views.py
@@ -41,14 +41,13 @@ def validation(key, value, arr):
 @app.route("/user", methods=["POST"])
 def create_user():
     request_data = {}
-    global user_id
-    user_id += 1
-    request_data["id"] = user_id
     try:
         request_data["name"] = request.get_json()["name"]
+        global user_id
+        user_id += 1
+        request_data["id"] = user_id
     except:
         return "Error bad request"
-
     USERS.append(request_data)
     return request_data
 
@@ -56,11 +55,11 @@ def create_user():
 @app.route("/category", methods=["POST"])
 def create_category():
     request_data = {}
-    global category_id
-    category_id += 1
-    request_data["id"] = category_id
     try:
         request_data["title"] = request.get_json()["title"]
+        global category_id
+        category_id += 1
+        request_data["id"] = category_id
     except:
         return "Error bad request"
     CATEGORIES.append(request_data)
@@ -70,14 +69,18 @@ def create_category():
 @app.route("/note", methods=["POST"])
 def create_note():
     request_data = request.get_json()
-    global note_id
-    note_id += 1
+
     try:
-        if not (validation("id", request.get_json()["user_id"], USERS) and validation("id", request.get_json()["category_id"], CATEGORIES)):
+        if not (validation("id", request.get_json()["user_id"], USERS) and validation("id",
+                                                                                      request.get_json()["category_id"],
+                                                                                      CATEGORIES)):
             return "Error, user or category is not found"
+        global note_id
+        note_id += 1
         request_data["id"] = note_id
         request_data["date_of_creating"] = datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")
         request_data["price"] = request.get_json()["price"]
+
     except:
         return "Error bad request"
 
@@ -100,3 +103,15 @@ def get_notes():
     return jsonify({"notes": NOTES})
 
 
+@app.route("/user-notes", methods=["POST"])
+def get_user_notes():
+    request_data = request.get_json()
+    try:
+        id_user = request_data["user"]
+    except:
+        return "Error bad request"
+    user_notes = []
+    for i in NOTES:
+        if i["user_id"] == id_user:
+            user_notes.append(i)
+    return jsonify(user_notes)
